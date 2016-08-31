10566dabf94f068fe50233230e84540aa7043e49
JAlmeida <JAlmeida@HSUKDL230000032>
2016-08-31 23:59:26 +0100

    First comit of FSharp test bed

---
+++ FSharp-TestBed/FSharp-TestBed/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+
+    // return an integer exit code
+
+printfn "hello world..."
+let s = System.Console.ReadLine()
